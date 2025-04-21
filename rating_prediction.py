37082a59de1be8b56850f6e2d1abb2450b88faaa
Itay Rubin <64592743+itay-rubin@users.noreply.github.com>
2025-04-21 16:59:15 -0400

    Update rating_prediction.py
    Replaced with my fine tuned model

--- rating_prediction.py
+++ rating_prediction.py
@@ -12,7 +12,7 @@ movie_summary = st.text_area("Movie Summary", height=200)
 
 if movie_summary:
     response = openai.chat.completions.create(
-        model="gpt-4o-mini", 
+        model="ft:gpt-4o-mini-2024-07-18:itay:lecture-21-exercise-1:BN50mIE4", 
         messages=[
             {"role": "system", "content": "You are a helpful assistant that predicts IMDb ratings based on plot synopsis of a movie."},
             {"role": "user", "content": f"Here is the plot synopsis: {movie_summary}\nRating:"}
